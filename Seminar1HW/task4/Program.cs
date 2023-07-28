3d7cef3773ddaaea915d7d9d1b2b91eadddd07f7
Andrew <flah504@gmail.com>
2023-07-28 19:58:17 +0300

    Разделил ДЗ по номерам уроков

---
+++ Seminar1HW/task4/Program.cs
@@ -0,0 +1,54 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+
+
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (number1 > number2)
+{
+    max = number1;
+    if (max > number3)
+    {
+        Console.Write($"{max} больше {number2} и {number3}");
+    }
+}
+if (number2 > number1)
+{
+    max = number2;
+    if (max > number3)
+    {
+        Console.Write($"{max} больше {number1} и {number3}");
+    }
+}
+if (number3 > number1)
+{
+    max = number3;
+    if (max > number2)
+    {
+        Console.Write($"{max} больше {number1} и {number2}");
+    }
+}
+if ((number1 == number2) && (number2 == number3))
+{
+    Console.Write("Числа равны");
+}
+if ((number1 == number2) || (number2 > number3))
+{
+    Console.Write($"Первое число: {number1} равно второму: {number2} и больше третьего: {number3}");
+}
+if ((number1 == number3) || (number3 > number2))
+{
+    Console.Write($"Первое число: {number1} равно третьему: {number3} и больше второго: {number2}");
+}
+if ((number2 == number3) || (number3 > number1))
+{
+    Console.Write($"Второе число: {number2} равно третьему: {number3} и больше первого: {number1}");
+}
---
+++ Seminar4HW/task25/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+int GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int FindDegreeOfNumber(int numberA, int numberB)
+{
+    int degree = 1;
+    int result = numberA;
+    while (degree < numberB)
+    {
+        degree++;
+        result *= numberA;
+        Console.WriteLine($"{numberA}^{degree} = {result}");
+    }
+    return result;
+}
+
+int numberA = GetInput("Введите число A: ");
+int numberB = GetInput("Введите степень числа A: ");
+FindDegreeOfNumber(numberA, numberB);
