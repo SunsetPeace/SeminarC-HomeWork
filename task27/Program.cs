ebc050ba4cc602ff61f734bd6d1c890a401b0db0
Andrew <flah504@gmail.com>
2023-07-28 19:52:53 +0300

    Выполнил ДЗ Семинар 4

---
+++ task27/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+int GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetSumInNumber(int number)
+{
+int result = 0;
+    while (number > 0)
+    {
+        int variable_num = number % 10;
+        result += variable_num;
+        number /= 10;
+    }
+    return result;
+}
+
+int number = GetInput("Введите число: ");
+Console.WriteLine($"Сумма цифр в числе {number} = {GetSumInNumber(number)}");
---
+++ task29/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+
+// int GetInput(string text)
+// {
+//     Console.Write(text);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// int[] arrayHandMade = new int[8];
+// Random rand = new Random();
+// for (int i = 0; i < arrayHandMade.Length; i++)
+//     arrayHandMade[i] = rand.Next(0, 100);
+// Console.WriteLine("[{0}]", string.Join(", ", arrayHandMade));
+
+int GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void GetArray(int quantity)
+{
+    int count = 1;
+    int GetInput(string text)
+    {
+        Console.Write(text);
+        return Convert.ToInt32(Console.ReadLine());
+    }
+    int[] arrayHandMade = new int[quantity];
+    for (int i = 0; i < arrayHandMade.Length; i++)
+        arrayHandMade[i] = GetInput($"Введите {count++} число: ");
+    Console.WriteLine("[{0}]", string.Join(", ", arrayHandMade));
+}
+
+int quantity = GetInput("Введите размер массива: ");
+GetArray(quantity);
